8dba293847c51777ea043ecd2d29231ec08ecdcb
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-10-14 20:12:46 +0800

    Add caching between pages

--- media/image.py
+++ media/image.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 st.title("Detect deepfake in Image")
 
+if "item" not in st.session_state:
+    st.session_state.item = None
+
 image_file = st.file_uploader("Browse Files", type=["jpeg", "jpg", "png"])
 
 if image_file:
-    st.image(image_file)
+    st.session_state.item = image_file
+
+if st.session_state.item:
+    st.image(st.session_state.item)
--- media/video.py
+++ media/video.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 st.title("Detect deepfake in Video")
 
+if "item" not in st.session_state:
+    st.session_state.item = None
+
 video_file = st.file_uploader("Browse Files", type=["mp4", "mkv"])
 
 if video_file:
-    st.image(video_file)
+    st.session_state.item = video_file
+
+if st.session_state.item:
+    st.image(st.session_state.item)
