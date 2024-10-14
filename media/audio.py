ae309f1f19a9f561024f9ea55b98173a1c2de5d2
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-10-14 20:23:45 +0800

    Fix caching across pages

--- media/audio.py
+++ media/audio.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title("Detect deepfake in Audio")
 
-if "item" not in st.session_state:
-    st.session_state.item = None
+if "audio" not in st.session_state:
+    st.session_state.audio = None
 
 audio_file = st.file_uploader("Browse Files", type=["mp3"])
 
 if audio_file:
-    st.session_state.item = audio_file
+    st.session_state.audio = audio_file
 
-if st.session_state.item:
-    st.image(st.session_state.item)
+if st.session_state.audio:
+    st.image(st.session_state.audio)
--- media/image.py
+++ media/image.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title("Detect deepfake in Image")
 
-if "item" not in st.session_state:
-    st.session_state.item = None
+if "image" not in st.session_state:
+    st.session_state.image = None
 
 image_file = st.file_uploader("Browse Files", type=["jpeg", "jpg", "png"])
 
 if image_file:
-    st.session_state.item = image_file
+    st.session_state.image = image_file
 
-if st.session_state.item:
-    st.image(st.session_state.item)
+if st.session_state.image:
+    st.image(st.session_state.image)
