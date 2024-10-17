d43d287e225fc618a1fe58d75b41e6fa9d174426
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-10-17 16:19:43 +0800

    Print output from cache

--- media/image.py
+++ media/image.py
@@ -6,47 +6,67 @@ from huggingface_hub import hf_hub_download
 from transformers import pipeline
 
 
-config = toml.load("config.toml")
-HUGGING_FACE_API_KEY = config["api"]["HUGGING_FACE_API_KEY"]
+def load_api_key() -> str:
+    config = toml.load("config.toml")
+    HUGGING_FACE_API_KEY = config["api"]["HUGGING_FACE_API_KEY"]
+    return HUGGING_FACE_API_KEY
 
-model_id = "SivaResearch/Fake_Detection"
-filenames = [
-    "config.json",
-    "deepfakeconfig.py",
-    "deepfakemodel.py",
-    "model.safetensors",
-    "pipeline.py",
-    "pytorch_model.bin",
-    "requirements.txt",
-]
+@st.cache_resource
+def load_model_resnetinception(API_KEY: str):
+    model_id = "SivaResearch/Fake_Detection"
+    filenames = [
+        "config.json",
+        "deepfakeconfig.py",
+        "deepfakemodel.py",
+        "model.safetensors",
+        "pipeline.py",
+        "pytorch_model.bin",
+        "requirements.txt",
+    ]
+    for filename in filenames:
+        downloaded_model_path = hf_hub_download(
+            repo_id=model_id,
+            filename=filename,
+            token=API_KEY
+        )
+    return pipeline(model="not-lain/deepfake", trust_remote_code=True)
 
-for filename in filenames:
-    downloaded_model_path = hf_hub_download(
-        repo_id=model_id,
-        filename=filename,
-        token=HUGGING_FACE_API_KEY
-    )
+def temp_path(image_file):
+    temp_dir = tempfile.mkdtemp()
+    temp_path = os.path.join(temp_dir, image_file.name)
+    with open(temp_path, "wb") as f:
+        f.write(image_file.getvalue())
+    return temp_path, temp_dir
 
-# pipe = pipeline("image-classification", model=model_id, device=-1, trust_remote_code=True)
-pipe = pipeline(model="not-lain/deepfake", trust_remote_code=True)
+def main() -> None:
+    # page title
+    st.title("Detect deepfake in Image")
 
+    # initialize cache
+    if "image" not in st.session_state:
+        st.session_state.image = None
+    if "result" not in st.session_state:
+        st.session_state.result = None
+    # store in image object
+    image_file = st.file_uploader("Browse Files", type=["jpeg", "jpg", "png", "webp"])
 
-st.title("Detect deepfake in Image")
+    # grab token
+    token = load_api_key()
 
-if "image" not in st.session_state:
-    st.session_state.image = None
+    # cache image
+    if image_file:
+        st.session_state.image = image_file
+        # display image
+        st.image(st.session_state.image)
+        path, dir = temp_path(st.session_state.image)
+        pipe = load_model_resnetinception(token)
+        result = pipe.predict(path)
+        st.write(result["confidences"])
+        st.session_state.result = result["confidences"]
 
-image_file = st.file_uploader("Browse Files", type=["jpeg", "jpg", "png", "webp"])
+    # pull image from cache
+    if not image_file and st.session_state.image:
+        st.image(st.session_state.image)
+        st.write(st.session_state.result)
 
-if image_file:
-    st.session_state.image = image_file
-
-if st.session_state.image:
-    st.image(st.session_state.image)
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_path = os.path.join(temp_dir, image_file.name)
-        with open(temp_path, "wb") as f:
-            f.write(image_file.getvalue())
-        # st.write(pipe(temp_path))
-        st.write(pipe.predict(temp_path))
+main()
