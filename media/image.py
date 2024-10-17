2a638f1ec22e2e278d19c4cecb580aebe4084e5b
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-10-17 19:08:33 +0800

    Add different models, Cache results, Replace results when new model is selected

--- media/image.py
+++ media/image.py
@@ -11,32 +11,75 @@ def load_api_key() -> str:
     HUGGING_FACE_API_KEY = config["api"]["HUGGING_FACE_API_KEY"]
     return HUGGING_FACE_API_KEY
 
+def temp_path(image_file: str) -> str:
+    temp_dir = tempfile.mkdtemp()
+    temp_path = os.path.join(temp_dir, image_file.name)
+    with open(temp_path, "wb") as f:
+        f.write(image_file.getvalue())
+    return temp_path, temp_dir
+
 @st.cache_resource
-def load_model_resnetinception(API_KEY: str):
-    model_id = "SivaResearch/Fake_Detection"
-    filenames = [
-        "config.json",
-        "deepfakeconfig.py",
-        "deepfakemodel.py",
-        "model.safetensors",
-        "pipeline.py",
-        "pytorch_model.bin",
-        "requirements.txt",
-    ]
+def load_model(API_KEY: str, option: str):
+    if option == "dima806/deepfake_vs_real_image_detection":
+        model_id = option
+        filenames = [
+            "config.json",
+            "model.safetensors",
+            "preprocessor_config.json",
+            "pytorch_model.bin",
+            "training_args.bin",
+        ]
+        pipe = pipeline("image-classification", model=model_id, device=-1)
+    if option == "Wvolf/ViT_Deepfake_Detection":
+        model_id = option
+        filenames = [
+            "config.json",
+            "preprocessor_config.json",
+            "pytorch_model.bin",
+            "training_args.bin",
+        ]
+        pipe = pipeline("image-classification", model=model_id, device=-1)
+    if option == "prithivMLmods/Deep-Fake-Detector-Model":
+        model_id = option
+        filenames = [
+            "config.json",
+            "model.safetensors",
+            "preprocessor_config.json",
+            "pytorch_model.bin",
+            "training_args.bin",
+        ]
+        pipe = pipeline("image-classification", model=model_id, device=-1)
+    if option == "not-lain/deepfake" or option == "SivaResearch/Fake_Detection":
+        model_id = option
+        filenames = [
+            "config.json",
+            "deepfakeconfig.py",
+            "deepfakemodel.py",
+            "model.safetensors",
+            "pipeline.py",
+            "pytorch_model.bin",
+            "requirements.txt",
+        ]
+        pipe = pipeline(model="not-lain/deepfake", trust_remote_code=True)
     for filename in filenames:
         downloaded_model_path = hf_hub_download(
             repo_id=model_id,
             filename=filename,
             token=API_KEY
         )
-    return pipeline(model="not-lain/deepfake", trust_remote_code=True)
+    return pipe
 
-def temp_path(image_file):
-    temp_dir = tempfile.mkdtemp()
-    temp_path = os.path.join(temp_dir, image_file.name)
-    with open(temp_path, "wb") as f:
-        f.write(image_file.getvalue())
-    return temp_path, temp_dir
+def display_result(token: str, option: str) -> str:
+    path, dir = temp_path(st.session_state.image)
+    pipe = load_model(token, option)
+    if option == "not-lain/deepfake" or option == "SivaResearch/Fake_Detection":
+        result = pipe.predict(path)
+        st.session_state.result = result["confidences"]
+        return result["confidences"]
+    else:
+        result = pipe(path)
+        st.session_state.result = result
+        return result
 
 def main() -> None:
     # page title
@@ -47,6 +90,19 @@ def main() -> None:
         st.session_state.image = None
     if "result" not in st.session_state:
         st.session_state.result = None
+
+    # model choice
+    option = st.selectbox(
+        "Select Model",
+        (
+            "not-lain/deepfake",
+            "SivaResearch/Fake_Detection",
+            "dima806/deepfake_vs_real_image_detection", 
+            "Wvolf/ViT_Deepfake_Detection", 
+            "prithivMLmods/Deep-Fake-Detector-Model",
+        ),
+    )
+    
     # store in image object
     image_file = st.file_uploader("Browse Files", type=["jpeg", "jpg", "png", "webp"])
 
@@ -58,15 +114,15 @@ def main() -> None:
         st.session_state.image = image_file
         # display image
         st.image(st.session_state.image)
-        path, dir = temp_path(st.session_state.image)
-        pipe = load_model_resnetinception(token)
-        result = pipe.predict(path)
-        st.write(result["confidences"])
-        st.session_state.result = result["confidences"]
+        # show result
+        result = display_result(token, option)
+        st.write(result)
 
     # pull image from cache
-    if not image_file and st.session_state.image:
+    elif st.session_state.image:
         st.image(st.session_state.image)
         st.write(st.session_state.result)
+        st.session_state.image = None
+        st.session_state.result = None
 
 main()
